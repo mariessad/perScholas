982ee0b6026f8f6dcd358903d11ae02d30f707d0
mariessad <74148632+mariessad@users.noreply.github.com>
2022-08-25 11:25:08 -0400

    knife JS animation on mouseover for all knife img elements

--- week3/day2/Movies_Media_queries/app.js
+++ week3/day2/Movies_Media_queries/app.js
@@ -5,7 +5,12 @@ let movieDivThree = document.querySelector(".movie-three");
 let movieTab = document.querySelector(".movie-tab");
 let movieTabOne = document.querySelector(".movie-tab-two");
 let movieTabTwo = document.querySelector(".movie-tab-three");
-// console.log(movieDiv);
+
+let knife = document.querySelector(".knife-icon");
+console.log(knife);
+let knifeOne = document.querySelector(".knife-icon-two");
+let knifeTwo = document.querySelector(".knife-icon-three");
+let knifeThree = document.querySelector(".knife-icon-one");
 
 const eventHandler = () => {
   //   alert("clicked!");
@@ -21,6 +26,19 @@ const eventHandler2 = () => {
   movieDivThree.classList.toggle("toggle-hide");
 };
 
+const eventSpin = () => {
+  knife.classList.toggle("spin");
+};
+const eventSpin1 = () => {
+  knifeOne.classList.toggle("spin");
+};
+const eventSpin2 = () => {
+  knifeTwo.classList.toggle("spin");
+};
+const eventSpin3 = () => {
+  knifeThree.classList.toggle("spin");
+};
+
 movieDiv.addEventListener("click", eventHandler);
 movieDivTwo.addEventListener("click", eventHandler1);
 movieDivThree.addEventListener("click", eventHandler2);
@@ -28,3 +46,8 @@ movieDivThree.addEventListener("click", eventHandler2);
 movieTab.addEventListener("click", eventHandler);
 movieTabOne.addEventListener("click", eventHandler1);
 movieTabTwo.addEventListener("click", eventHandler2);
+
+knife.addEventListener("mouseover", eventSpin);
+knifeOne.addEventListener("mouseover", eventSpin1);
+knifeTwo.addEventListener("mouseover", eventSpin2);
+knifeThree.addEventListener("mouseover", eventSpin3);
