00125e8471083e8ed3c9d8878d87afb3e91dccca
mariessad <74148632+mariessad@users.noreply.github.com>
2022-08-26 13:57:18 -0400

    monster only animates after game starts by button press

--- week3/day4/game_project/app.js
+++ week3/day4/game_project/app.js
@@ -3,7 +3,7 @@
 //on key press event, trigger jump animation class
 
 const bunny = document.getElementById("character");
-const food = document.getElementById("health");
+// const food = document.getElementById("health");
 const monster = document.getElementById("obstacle");
 const score = document.getElementById("score");
 const button = document.querySelector("button");
@@ -24,6 +24,7 @@ document.addEventListener("keypress", () => {
 // start button
 
 button.addEventListener("click", () => {
+  monster.style.animation = "monster 1.33s infinite";
   //update state of the game
 
   setInterval(() => {
@@ -34,22 +35,24 @@ button.addEventListener("click", () => {
     const monsterLeft = parseInt(
       window.getComputedStyle(monster).getPropertyValue("left")
     );
-    const foodLeft = parseInt(
-      window.getComputedStyle(food).getPropertyValue("left")
-    );
+    // const foodLeft = parseInt(
+    //   window.getComputedStyle(food).getPropertyValue("left")
+    // );
     //   console.log(bunnyTop);
-    if (monsterLeft < 0) {
-      monster.display = "none";
+    if (monsterLeft <= 20) {
+      monster.style.display = "none";
     } else {
       monster.style.display = "";
     }
-    if (foodLeft < 0) {
-      food.display = "none";
-    } else {
-      food.style.display = "";
-    }
+    // if (foodLeft < 0) {
+    //   food.display = "none";
+    // } else {
+    //   food.style.display = "";
+    // }
+    console.log(monsterLeft);
+    console.log(bunnyTop);
 
-    if (monsterLeft < 50 && monsterLeft > 0 && bunnyTop > 150) {
+    if (monsterLeft < 30 && monsterLeft > 0 && bunnyTop > 150) {
       alert(`You got a score of ${score.innerText}\n\n Play again?`);
       location.reload();
     }
