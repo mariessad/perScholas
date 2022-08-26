020ed8391e017593a7a0e292d927daa34a0ed29c
mariessad <74148632+mariessad@users.noreply.github.com>
2022-08-26 12:51:14 -0400

    get elements, add jump function and keypress event listener

--- week3/day4/game_project/app.js
+++ week3/day4/game_project/app.js
@@ -1,1 +1,21 @@
-// side scroll platform game
+// side scroll game
+
+//on key press event, trigger jump animation class
+
+const bunny = document.getElementById("character");
+const food = document.getElementById("health");
+const monster = document.getElementById("obstacle");
+const score = document.getElementById("score");
+
+const jump = () => {
+  bunny.classList.add("jump-animation");
+  setTimeout(() => {
+    bunny.classList.remove("jump-animation");
+  }, 500);
+};
+
+document.addEventListener("keypress", () => {
+  if (!bunny.classList.contains("jump-animation")) {
+    jump();
+  }
+});
