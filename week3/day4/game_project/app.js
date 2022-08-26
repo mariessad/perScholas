bb891c40b27a3bb2354290d61f0b588adf0994bd
mariessad <74148632+mariessad@users.noreply.github.com>
2022-08-26 13:39:56 -0400

    add functionality, add start button so score only starts counting once game starts

--- week3/day4/game_project/app.js
+++ week3/day4/game_project/app.js
@@ -6,6 +6,7 @@ const bunny = document.getElementById("character");
 const food = document.getElementById("health");
 const monster = document.getElementById("obstacle");
 const score = document.getElementById("score");
+const button = document.querySelector("button");
 
 const jump = () => {
   bunny.classList.add("jump-animation");
@@ -18,4 +19,43 @@ document.addEventListener("keypress", () => {
   if (!bunny.classList.contains("jump-animation")) {
     jump();
   }
+});
+
+// start button
+
+button.addEventListener("click", () => {
+  //update state of the game
+
+  setInterval(() => {
+    score.innerText++;
+    const bunnyTop = parseInt(
+      window.getComputedStyle(bunny).getPropertyValue("top")
+    );
+    const monsterLeft = parseInt(
+      window.getComputedStyle(monster).getPropertyValue("left")
+    );
+    const foodLeft = parseInt(
+      window.getComputedStyle(food).getPropertyValue("left")
+    );
+    //   console.log(bunnyTop);
+    if (monsterLeft < 0) {
+      monster.display = "none";
+    } else {
+      monster.style.display = "";
+    }
+    if (foodLeft < 0) {
+      food.display = "none";
+    } else {
+      food.style.display = "";
+    }
+
+    if (monsterLeft < 50 && monsterLeft > 0 && bunnyTop > 150) {
+      alert(`You got a score of ${score.innerText}\n\n Play again?`);
+      location.reload();
+    }
+
+    //   if ((score.innerText = 0)) {
+    //     alert("game over");
+    //   }
+  }, 50);
 });
